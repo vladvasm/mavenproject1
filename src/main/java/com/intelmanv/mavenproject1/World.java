a3e1974cacae4d013aa155721a7ab8a6cf4c0c6b
vladvasm <86705129+vladvasm@users.noreply.github.com>
2021-08-16 02:48:44 +0300

    add World.java

---
+++ src/main/java/com/intelmanv/mavenproject1/World.java
@@ -0,0 +1,11 @@
+package com.intelmanv.mavenproject1;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class World {
+	public static void printStr(String myStr) {
+		System.out.println("World of , "+myStr);
+	}
+}
